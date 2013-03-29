dfab94d534d766cddf3468d4e39c33d22ae34abf
shrikrishnaholla <shrikrishna.holla@gmail.com>
2013-03-29 17:12:34 +0530

    Added progress bar and documentation

--- src/webapp/profilefetcher.py
+++ src/webapp/profilefetcher.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+"""The fetcher of LinkedIn profiles. This module acts as the controller for all operations related to 
+crawling the LinkedIn profiles"""
 import urllib2
 import re
 import crawler
@@ -5,33 +8,39 @@ import argparse
 import sys
 
 def fetchProfiles(initURL, maxcount):
+    """Given the URL from where to initiate the crawling, it first fetches the webpage, sends it to
+    the crawler for scraping data from the webpage. Not only that, it also reads all the public profile
+    urls present in the current page and adds them to the list. In subsequent iterations, it will fetch
+    the LinkedIn profiles of people associated with these urls. The iteration continues for the number of
+    times specified by maxcount"""
     count = 0
     links = set([initURL])
-    fetched = list()
+    waitinglist = list()
 
     while count< maxcount:
-        count+=1
+        count += 1
 
         while True:
             newreq = links.pop()
-            if newreq not in fetched:
-                fetched.append(newreq)
+            if newreq not in waitinglist:   # If the url hasn't be used already, add it to the waiting list
+                waitinglist.append(newreq)
                 break
 
-        page = urllib2.urlopen(fetched[-1]).read()
+        page = urllib2.urlopen(waitinglist[-1]).read() # Fetch the web page from the url just appended
 
-        crawler.contentExtractor(page, fetched[-1])
+        crawler.contentExtractor(page, waitinglist[-1]) # Send the page and the url for scraping
 
         links.update(re.findall(r'http://.*linkedin.com/pub/(?:[a-z]*[-]?)*(?:/?[0-9]?[a-z]?)*\?trk=pub-pbmap', page))
+        # Get all the urls present in this web page
 
         links = set([link.strip('"') for link in links])
-        #links = set(links) 
 
-        if maxcount % count == 0:
-            sys.stdout.write('\r'+'='*int((count*10.0/maxcount)*10.0)+'>'+ str((count*100.0/maxcount))+'%')
+        percentage = int(count*100.0/maxcount)    # Progress bar
+        sys.stdout.write('\r'+'='*percentage+'>'+' '*(101-percentage) +str(percentage)+'%')
+        sys.stdout.flush()
 
 def acceptCLArguments():
-    # Initializing parser for accepting command line arguements
+    """Initializing parser for accepting command line arguements"""
     parser = argparse.ArgumentParser(
         description="""Build database of LinkedIn public profiles by crawling through their pages""")
 
@@ -50,3 +59,4 @@ def acceptCLArguments():
 if __name__ == '__main__':
     args = acceptCLArguments()
     fetchProfiles(args.url, args.number)
+    print
